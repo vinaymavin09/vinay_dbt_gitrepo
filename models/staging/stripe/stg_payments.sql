3ef4e5007f21b3a13a89fae3fb0fa197f74c8da2
vinay.reddy@mediamint.com <vinay.reddy@mediamint.com>
2024-12-31 04:22:03 +0000

    awsd

---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,14 @@
+with payments as (
+  
+  select
+        id as payment_id,
+        orderid,
+        paymentmethod,
+        status,
+        amount,
+        created
+
+    from raw.stripe.payment
+)
+
+select * from payments
