9476182d804c7f78ee8923e2542c99741d5af865
vinay.reddy@mediamint.com <vinay.reddy@mediamint.com>
2024-12-31 04:06:18 +0000

    202412310936

--- models/Marts/core/dim_customers.sql
+++ models/Marts/core/dim_customers.sql
@@ -1,24 +1,16 @@
-{{ config(materialized='table') }}
 
-with customers as
+-- these are the reference of stg customer ans stg_orders
+with 
+customers as
 (
-    select id as customer_id,
-    first_name,
-    last_name
-    from
-    raw.jaffle_shop.customers
+    select * from {{ref("stg_customers")}}
 ),
 
 orders as
 (
-    select id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-    from raw.jaffle_shop.orders
-
+    select * from {{ref("stg_orders")}}
 ),
-customer_orders as (
+ customer_orders as (
     select  customer_id,
     min(order_date) as first_order_date,
     max(order_date) as recent_order_date,
